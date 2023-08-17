08f356f075eb7da4f63c53482f2ca2a9a9e33abf
rbc33 <ricardobenthem@gmail.com>
2023-08-17 02:45:22 +0200

    now refresh values in a static window

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 playing around with py2app and psutils
 
 python3 setup.py py2app
+
+
+requirements:
+
+altgraph==0.17.3
+macholib==1.16.2
+modulegraph==0.19.5
+psutil==5.9.5
+py2app==0.28.6
+setuptools==68.1.0; python_version >= '3.8'
--- monitor2app.py
+++ monitor2app.py
@@ -1,16 +1,29 @@
+
+import os
+import time
 import psutil
+import sys
+
+def clear_screen():
+    os.system('clear')  # For macOS and Linux
 
 def main():
-    memory_percent = psutil.virtual_memory().percent
-    cpu_percent = psutil.cpu_percent()
-    battery_percent = psutil.sensors_battery().percent if hasattr(psutil, 'sensors_battery') else None
+    while True:
+        memory_percent = psutil.virtual_memory().percent
+        cpu_percent = psutil.cpu_percent()
+        battery_percent = psutil.sensors_battery().percent if hasattr(psutil, 'sensors_battery') else None
 
-    print(f"Memory Percent: {memory_percent}%")
-    print(f"CPU Percent: {cpu_percent}%")
-    if battery_percent is not None:
-        print(f"Battery Percent: {battery_percent}%")
-    else:
-        print("Battery status not available.")
+        clear_screen()  # Clear the entire console screen
+
+        print(f"Memory Percent: {memory_percent}%")
+        print(f"CPU Percent: {cpu_percent}%")
+
+        if battery_percent:
+            print(f"Battery Percent: {battery_percent}%")
+        else:
+            print("Battery status not available.")
+
+        time.sleep(0.5)  # Wait for 0.5 seconds before the next update
 
 if __name__ == "__main__":
     main()
